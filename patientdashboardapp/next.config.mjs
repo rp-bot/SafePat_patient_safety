7e71e8344b2709f92d937afa27fbe65d86337e59
rp-bot_win10 <legacy.business18@gmail.com>
2024-09-29 04:47:23 -0400

    added build

--- patientdashboardapp/next.config.mjs
+++ patientdashboardapp/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Remove the experimental section if it's empty
+	// Remove the experimental section if it's empty
 };
 
 export default nextConfig;
